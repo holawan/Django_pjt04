f7e04bba1d71b32a129bff4e0e5aebd7f819502e
holawan <asdf134652@gmail.com>
2022-04-22 13:55:28 +0900

    [update] model manytomanyfields 위치 변경으로 fixtures와 형식 맞춰줌

--- movies/models.py
+++ movies/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Actor(models.Model) :
+    #배우 이름 
+    name = models.CharField(max_length=100)
+    
+
 class Movie(models.Model) :
     #영화 제목 
     title = models.CharField(max_length=100)
@@ -11,15 +16,13 @@ class Movie(models.Model) :
     release_date = models.DateTimeField()
     #포스터 주소 
     poster_path = models.TextField()
-    
-class Actor(models.Model) :
-    #배우 이름 
-    name = models.CharField(max_length=100)
-    movies = models.ManyToManyField(Movie,related_name='starring_actors')
+    actors = models.ManyToManyField(Actor,related_name='starring_movie')
+
+
 
     
 
 class Review(models.Model) :
+    movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    movies = models.ForeignKey(Movie,on_delete=models.CASCADE)
