ba7fde54b32711c826e8e0b9a3c654a0a634d32c
holawan <asdf134652@gmail.com>
2022-04-22 13:25:36 +0900

    [ADD] ERD에 맞춰서 모델 구성

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model) :
+    #영화 제목 
+    title = models.CharField(max_length=100)
+    #줄거리 
+    overview = models.TextField()
+    #개봉일 
+    release_date = models.DateTimeField()
+    #포스터 주소 
+    poster_path = models.TextField()
+    
+class Actor(models.Model) :
+    #배우 이름 
+    name = models.CharField(max_length=100)
+    movies = models.ManyToManyField(Movie,related_name='starring_actors')
+
+    
+
+class Review(models.Model) :
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    movies = models.ForeignKey(Movie,on_delete=models.CASCADE)
