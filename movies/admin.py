53d28e80c3d9199a28df5d85fb4952c53d9186bd
holawan <asdf134652@gmail.com>
2022-04-22 14:04:09 +0900

    [update] admin page custom

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Actor,Movie,Review
 # Register your models here.
+
+class ActorAdmin(admin.ModelAdmin) :
+
+    model = Actor
+    list_display = ('name',)
+
+
+class MovieAdmin(admin.ModelAdmin) :
+
+    model = Movie
+    list_display = ('title','overview',)
+
+
+class ReviewAdmin(admin.ModelAdmin) :
+
+    model = Actor
+    list_display = ('title','content')
+
+admin.site.register(Actor,ActorAdmin)
+admin.site.register(Movie,MovieAdmin)
+admin.site.register(Review,ReviewAdmin)
--- movies/models.py
+++ movies/models.py
@@ -6,6 +6,8 @@ class Actor(models.Model) :
     #배우 이름 
     name = models.CharField(max_length=100)
     
+    def __str__(self) :
+        return f'배우이름 : {self.name}'
 
 class Movie(models.Model) :
     #영화 제목 
@@ -18,11 +20,13 @@ class Movie(models.Model) :
     poster_path = models.TextField()
     actors = models.ManyToManyField(Actor,related_name='starring_movie')
 
+    def __str__(self) :
+        return f'영화제목 : {self.title} 줄거리 : {self.overview}'
 
-
-    
 
 class Review(models.Model) :
     movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
+    def __str__(self) :
+        return f'영화제목 : {self.title} 줄거리 : {self.content}'
